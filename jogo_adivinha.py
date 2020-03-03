28130417966674009ca45ffab19c6455d237e6d0
Guilherme <guilherme.amadi@gmail.com>
2020-03-03 19:15:09 -0300

    include for statemment

--- jogo_adivinha.py
+++ jogo_adivinha.py
@@ -5,8 +5,8 @@ print('***********************\n')
 numero_secreto = 42
 numero_tentativas = 3
 rodada = 1
-
-while (rodada <= numero_tentativas):
+for rodada in range(1, numero_tentativas+1):
+###while (rodada <= numero_tentativas):
     print(f"Rodada {rodada} de {numero_tentativas}")
     #recebeo chute
     chute = int(input("digite um numero: \n"))
@@ -26,5 +26,5 @@ while (rodada <= numero_tentativas):
     elif maior:
         print("você errou ! digitou um numero maior que o secreto.")
 
-    rodada = rodada +1
+   ### rodada = rodada +1
 print("Fim de Jogo")
