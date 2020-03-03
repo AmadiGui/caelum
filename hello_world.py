3aa5a01ef24011002901ee884b3c0942e0741b97
Guilherme <guilherme.amadi@gmail.com>
2020-03-03 19:03:06 -0300

    First commit caelum

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#variavel com texto
+texto = "hello world"
+print(texto)
+
+#variavel com numero
+conta = 5 + 5
+print(conta)
+
+#print contatenado
+print(conta, texto)
+
+#print duas linhas
+duas_linhas = "ola\nmunnnndo"
+print(duas_linhas)
+
+print(conta, texto, end='', sep='---', flush=True )
+
+
---
+++ jogo_adivinha.py
@@ -0,0 +1,30 @@
+print('\n***********************')
+print('* jogo da adivinhacao *')
+print('***********************\n')
+
+numero_secreto = 42
+numero_tentativas = 3
+rodada = 1
+
+while (rodada <= numero_tentativas):
+    print(f"Rodada {rodada} de {numero_tentativas}")
+    #recebeo chute
+    chute = int(input("digite um numero: \n"))
+   
+    print("você digitou", chute)
+
+    igual = (chute == numero_secreto)
+    maior = (chute > numero_secreto)
+    menor = (chute < numero_secreto)
+
+    #compara o chute com o numero secreto
+    if  igual:
+        print("você acertou o numero secreto.")
+        break
+    elif menor:
+        print("você errou ! digitou um numero menor que o secreto.") 
+    elif maior:
+        print("você errou ! digitou um numero maior que o secreto.")
+
+    rodada = rodada +1
+print("Fim de Jogo")
