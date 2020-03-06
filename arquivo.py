054bfd0e2b0e67c434840972597e595f8c7c6c84
Guilherme <guilherme.amadi@gmail.com>
2020-03-05 22:53:29 -0300

    aula0503

---
+++ arquivo.py
@@ -0,0 +1,9 @@
+
+arquivo = open("palavras.txt", "w")
+
+arquivo.write('banana\n')
+arquivo.write('maca\n')
+arquivo.write('melancia\n')
+arquivo.write('abacaxi\n')
+
+arquivo.close()
--- forca.py
+++ forca.py
@@ -1,10 +1,26 @@
+import random
+
 def jogar():
+    imprime_mensagem_abertura()
+
+def imprime_mensagem_abertura():
     print('*********************************')
     print('***Bem vindo ao jogo da Forca!***')
     print('*********************************')
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
@@ -15,6 +31,7 @@ def jogar():
     while (not enforcou and not acertou):
 
         chute = input("Qual letra? ")
+        chute = chute.strip().upper()
 
         if (chute in palavra_secreta):
 
