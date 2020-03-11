8b7b60563be76a9a37468407b23304bb8a5d0f26
Guilherme <guilherme.amadi@gmail.com>
2020-03-10 22:57:59 -0300

    aula1003

--- banco/conta.py
+++ banco/conta.py
@@ -1,10 +1,33 @@
 
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+
+    identificador = 0
+
+    __slots__ = ['_numero', '_titular', '_saldo', '_limite']
+
+    
+
+    def __init__(self, numero, titular, saldo, limite=1000.00):
+        self._numero = numero
+        self._titular = titular
+        self._saldo = saldo
+        self._limite = limite
+        Conta.identificador += 1
+
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @saldo.setter
+    def saldo(self, saldo):
+        if(self._saldo < 0):
+            print("saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
 
     def deposita(self, valor):
         self.saldo += valor
@@ -18,7 +41,8 @@ class Conta:
 
 
     def extrato(self):
-        print(f'numero.{self.numero}\n saldo.{self.saldo}')
+        print(f'numero: {self.numero}\nsaldo: {self.saldo}')
+#        print(f'saldo: {self.saldo}')
 
     def transfere_para(self, destino, valor):
         retirou = self.saca(valor)
--- banco/conta_teste.py
+++ banco/conta_teste.py
@@ -1,11 +1,11 @@
 from conta import Conta
 
-conta = Conta('123-4', 'João', 120.0, 1000.0)
-conta2 = Conta('123-5', 'Maria', 300.0 , 1000.0)
+conta = Conta('123-4', 'João', 100, 1000.0)
+conta2 = Conta('123-5', 'Maria', 100, 1000.0)
+conta3 = Conta('123-5', 'Maria', 100, 1000.0)
 
 #conta.saca(20.0)
-conta.transfere_para(conta2, 10.0)
+#conta.transfere_para(conta2, 10.0)
 
-conta.extrato()
-
-
+#conta.extrato()
+print(Conta.identificador)
